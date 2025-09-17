945f75ea82ca216a72559b634f5f6ee488e3e0c2
Aaron Witten <a.witten94@gmail.com>
2025-09-17 16:34:27 +1200

    Changed settings in Main class to final variables and included those variables in the screen properties.

--- PongGame/src/game/Pong.java
+++ PongGame/src/game/Pong.java
@@ -3,8 +3,11 @@ import javax.swing.JFrame;
 
 public class Pong extends JFrame{
 	public Pong() { 
-		setTitle("Pong");
-		setSize(800, 600);
+		final int SCREEN_WIDTH = 800;
+		final int SCREEN_HEIGHT = 600;
+		final String TITLE = "Pong";
+		setTitle(TITLE);
+		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
